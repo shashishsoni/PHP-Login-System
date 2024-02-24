70611c965f683954fe356e25fd8835358b8b7b1f
shashish <shshshsoni2003@tutanota.com>
2024-02-24 19:50:16 +0530

    edited the code and added a ajax page

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,9 @@
 <!-- UIkit JS -->
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit-icons.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+
+<script src="login-page/assets/js/main.js"></script>
